9478d6f9ed9869c67b4b7039182d4cd79b71b683
Endel Dreyer <endel.dreyer@gmail.com>
2016-09-21 18:30:50 +0200

    add smart overwriting output system (--overwrite/--interactive)

--- src/command.ts
+++ src/command.ts
@@ -1,13 +1,19 @@
 /*jshint node:true*/
 /// <reference path="../typings/index.d.ts" />
 
+declare module "readline-sync" {
+    var foo:any;
+    export = foo;
+};
+
 import parse from './parse/index';
 import { emit, EmitterOptions } from './emit/emitter';
 import fs = require('fs-extra');
 import path = require('path');
 import parseArgs = require('minimist');
+import readlineSync = require('readline-sync');
 
-const util = require('util');
+import { getLockTimestamp, updateLockTimestamp } from "./tool/lock";
 
 function readdir(dir: string, prefix = '', result: string[] = []): string[] {
     fs.readdirSync(dir).forEach(file => {
@@ -24,7 +30,14 @@ function readdir(dir: string, prefix = '', result: string[] = []): string[] {
 
 
 function displayHelp(): void {
-    console.log('usage: as3-to-typescript <sourceDir> <outputDir> [--commonjs] [--bridge name]');
+    console.log('usage: as3-to-typescript <sourceDir> <outputDir> [options]');
+    console.log('');
+    console.log('   Available options:');
+    console.log('       --commonjs: ignore AS3 packages on TypeScript output.');
+    console.log('       --bridge: use custom a output visitor. (--bridge createjs)');
+    console.log('       --interactive: ask to write the output in case it was manually modified');
+    console.log('       --overwrite: overwrite output files');
+    console.log('');
 }
 
 export function run(): void {
@@ -49,9 +62,23 @@ export function run(): void {
         if (!fs.statSync(outputDir).isDirectory()) {
             throw new Error('invalid ouput dir');
         }
-        fs.removeSync(outputDir);
+    } else {
+        fs.mkdirSync(outputDir);
     }
-    fs.mkdirSync(outputDir);
+
+    let bridge = (args['bridge'] && require("./bridge/" + args['bridge']).default) || null;
+    let overwrite = !!args['overwrite'];
+    let commonjs = args['commonjs'];
+    let interactive = args['interactive'];
+
+    if (overwrite && interactive) {
+        console.error("You can't have '--overwrite' and '--interactive' at the same time.");
+        process.exit();
+    }
+
+    // get last conversion timestamp
+    let previousLockTimestamp = getLockTimestamp(outputDir);
+    let nextLockTimestamp = Math.floor((new Date()).getTime() / 1000);
 
     let files = readdir(sourceDir).filter(file => /.as$/.test(file));
     let number = 0;
@@ -70,33 +97,44 @@ export function run(): void {
             definitionsByNamespace[ ns ] = [ ];
         }
 
-        definitionsByNamespace[ ns ].push( identifier )
+        definitionsByNamespace[ ns ].push( identifier );
     });
-
-    let bridge = (args['bridge'] && require("./bridge/" + args['bridge']).default) || null;
 
     let emitterOptions: EmitterOptions = {
         lineSeparator: '\n',
-        useNamespaces: (!args['commonjs']),
+        useNamespaces: !commonjs,
         bridge: bridge,
         definitionsByNamespace: definitionsByNamespace
     };
 
     files.forEach(file => {
         console.log('(' + ( number + 1 ) + '/' + length + ') \'' + file + '\'');
-        let content = fs.readFileSync(path.resolve(sourceDir, file), 'UTF-8');
+
+        let fileTs = file.replace(/.as$/, '.ts');
+        let inputFile = path.resolve(sourceDir, file);
+        let outputFile = path.resolve(outputDir, fileTs);
+
+        if (!overwrite && fs.existsSync(outputFile)) {
+            let stat = fs.statSync(outputFile)
+            if (previousLockTimestamp.getTime() !== stat.mtime.getTime()) {
+                if (interactive && !readlineSync.keyInYN(`"${ fileTs }" has been modified. Overwrite it?`)) {
+                    return;
+                }
+            }
+        }
+
+        let content = fs.readFileSync(inputFile, 'UTF-8');
         let ast = parse(path.basename(file), content);
-
-        // console.log(util.inspect(ast, { showHidden: true, depth: null }));
-
         let contents = emit(ast, content, emitterOptions);
 
         if (bridge && bridge.postProcessing) {
             contents = bridge.postProcessing(emitterOptions, contents);
         }
 
-        fs.outputFileSync(path.resolve(outputDir, file.replace(/.as$/, '.ts')), contents);
+        fs.outputFileSync(outputFile, contents);
+        fs.utimesSync(outputFile, nextLockTimestamp, nextLockTimestamp);
         number ++;
     });
-}
 
+    updateLockTimestamp(outputDir, nextLockTimestamp);
+}
