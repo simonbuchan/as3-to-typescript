635597fe9f4e878b95f0e99435f0bc14c3b7369f
Alejandro Santander <palebluedot@gmail.com>
2017-04-14 14:41:06 -0300

    Add trace visitor

---
+++ src/custom-visitors/trace/index.ts
@@ -0,0 +1,23 @@
+/**
+ * Created by palebluedot on 3/24/17.
+ */
+
+/**
+ * Replace trace() for console.log()
+ */
+
+import Node, { createNode } from "as3-to-ts/src/syntax/node";
+import Emitter, { EmitterOptions } from "as3-to-ts/src/emit/emitter";
+
+function visit (emitter: Emitter, node: Node): boolean {
+    return false;
+}
+
+const identifierMap: { [id: string]: string } = {
+    'trace': 'console.log'
+};
+
+export default {
+    visit: visit,
+    identifierMap: identifierMap,
+}
