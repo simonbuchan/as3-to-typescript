1c5614d1b2119bed1b34b6b748f3fc785bea831a
Alejandro Santander <palebluedot@gmail.com>
2017-04-24 12:56:51 -0300

    Fix includes

--- src/custom-visitors/include/index.ts
+++ src/custom-visitors/include/index.ts
@@ -26,6 +26,8 @@ function postProcessing (emitterOptions: EmitterOptions, contents: string): stri
     if(!snippets || snippets.length == 0) {
         return contents;
     }
+    console.log("-> include visitor - postprocessing()");
+    console.log("    include statements: " + snippets);
 
     // For each identified snippet...
     let acumSnippetContents:string = "";
@@ -33,20 +35,31 @@ function postProcessing (emitterOptions: EmitterOptions, contents: string): stri
 
         // Identify the actual path.
         const snippet:string = snippets[i];
-        const pathIncludingQuotes:string = snippet.match(/(["'])(?:(?=(\\?))\2.)*?\1/gm)[0];
+        const pathsIncludingQuotes:string[] = snippet.match(/(["'])(?:(?=(\\?))\2.)*?\1/gm);
+        console.log("    include paths: " + pathsIncludingQuotes);
+        if(!pathsIncludingQuotes || pathsIncludingQuotes.length == 0) {
+            continue;
+        }
+        const pathIncludingQuotes = pathsIncludingQuotes[0];
         const snpPath = pathIncludingQuotes.substring(1, pathIncludingQuotes.length - 1);
-        const tsPath = snpPath.split(".")[0] + ".ts";
-        // console.log("snippet: " + tsPath);
+        console.log("    snp path: " + snpPath);
+        const tsPath = snpPath.replace(/.snp$/, '.ts');
+        console.log("    ts path: " + tsPath);
+        const tsPathNonRelative = tsPath.replace("../", '').replace("./", '');
+        console.log("    tsPathNonRelative: " + tsPathNonRelative);
 
         // Load the contents of the snippet.
-        let snippetContent:string = "<<< INCLUDE CONTENT NOT FOUND: '" + tsPath + "' >>>";
+        let snippetContent:string = "<<< INCLUDE CONTENT NOT FOUND: '" + tsPathNonRelative + "' >>>";
         try {
-            const finalPath = path.resolve(emitterOptions.includePath, tsPath);
+            console.log("    (emitterOptions) include path: " + emitterOptions.includePath);
+            console.log("    pwd: " + __dirname);
+            const finalPath = path.resolve(emitterOptions.includePath, tsPathNonRelative);
+            console.log("    final path: " + finalPath);
             const cont = fs.readFileSync(finalPath);
             snippetContent = cont;
         }
         catch(e) {
-            console.log("include visitor - *** WARNING *** Snippet content not converted/loaded yet.");
+            console.log("    include visitor - *** WARNING *** Snippet content not converted/loaded yet.");
         }
         // console.log("snippetContent: " + snippetContent);
 
